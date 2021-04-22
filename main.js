cb28ebb81949e63eb31182381836e2cb37a44316
Abhijeet-prabhakar <beastgamerabhijeet01@gmail.com>
2021-04-22 13:45:56 +0530

    nice its working

---
+++ main.js
@@ -0,0 +1,8 @@
+// var x = getElementById("search").value;
+
+// if (x == 'dog')
+// 	window.open("./https://www.stthomasschoolranchi.com");
+// console.log(search);
+// if (search=='st.thomas.school') {
+// 	window.open("https://www.stthomasschoolranchi.com/");
+// }
