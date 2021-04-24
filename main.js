53da1932b2d7feedcdb4c2120d21512208cfd6b4
Abhijeet-prabhakar <beastgamerabhijeet01@gmail.com>
2021-04-24 14:18:27 +0530

    update idk the number

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-// var x = getElementById("search").value;
+function returnText() {
+    var useremail = document.getElementById("email.client");
+    alert(input);
+    // var mainbutton = document.getElementById('mainbutton');
+    // console.log(input);
+    // result.textContent = "Hey its sent";
+    // if (nameField.length < 3) {
+    //     useremail.textContent = 'Username must contain at least 3 characters';
+    //     //alert('Username must contain at least 3 characters');
+    // } else {
+    //     useremail.textContent = 'Your username is: '+ useremail;
+    //     //alert(nameField);
+    // }
 
-// if (x == 'dog')
-// 	window.open("./https://www.stthomasschoolranchi.com");
-// console.log(search);
-// if (search=='st.thomas.school') {
-// 	window.open("https://www.stthomasschoolranchi.com/");
-// }
+    // var  mainbutton = document.getElementById('subButton');
+    // mainbutton.addEventListener('click', getUserName, false); 
+    document.write("hello world");
+
+}
